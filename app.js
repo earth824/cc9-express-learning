f33661d98db44f8cbfc5850b8e86e52f0d89f3a6
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-09-07 18:44:13 +0700

    complete

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); // Content-Type application/x-www-form-urlencoded
+// false : lib qs JSON.parse username=12345&password=67890 to Javascript Object
+// true : lib querystring JSON.parse => username=12345&password=67890 to Javascript Object
+
+// Content-Type application/json
+app.use(express.json()); // => convert JSON string to Javascript Object
+
+// Content-Type multipart/form-data use external middleware such as busboy, formidable, multer
+
+// Client send request
+// axios.post('/upload', new FormData(), { headers: { ['Content-Type']: 'multipart/form-data' } });
+
 // app.use((req, res, next) => {
 //   console.log('Middleware 1');
 //   req.test = 'TEstTEst';
@@ -107,50 +119,90 @@ const app = express();
 // app.put('/products', (req, res, next) => {});
 // app.delete('/prosucts', (req, res, next) => {});
 
-const router = express.Router();
-router.get('/profile', (req, res, next) => {}); // GET /users/profile     xxx GET /profile
-router.post('/create', (req, res, next) => {}); // POST /users/create      xxx POST /create
+// const router = express.Router();
+// router.get('/profile', (req, res, next) => {}); // GET /users/profile     xxx GET /profile
+// router.post('/create', (req, res, next) => {}); // POST /users/create      xxx POST /create
 
-app.use('/users', router);
-app.use(
-  '/bookings',
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {}
-);
+// app.use('/users', router);
+// app.use(
+//   '/bookings',
+//   (req, res, next) => {
+//     next();
+//   },
+//   (req, res, next) => {
+//     next();
+//   },
+//   (req, res, next) => {
+//     next();
+//   },
+//   (req, res, next) => {}
+// );
 
-const productRouter = express.Router();
-productRouter.get('/', (req, res, next) => {
-  // GET /products
-  throw new Error('Throw error from GET /products');
-  // throw new CustomError('Throw error from GET /products', 400);
-  // next('errrrrrr');
+// const productRouter = express.Router();
+// productRouter.get('/', (req, res, next) => {
+//   // GET /products
+//   throw new Error('Throw error from GET /products');
+//   // throw new CustomError('Throw error from GET /products', 400);
+//   // next('errrrrrr');
+// });
+// productRouter.post('/', (req, res, next) => {
+//   try {
+//     // connected to database  to create new product
+//     // asynchronous operation
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+// productRouter.put('/', (req, res, next) => {});
+// productRouter.delete('/', (req, res, next) => {});
+
+// app.use('/products', productRouter);
+
+// app.use((req, res, next) => {});
+
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   res.status(err.statusCode || 500).json({ aaaaaaa: err.message });
+// });
+
+// app.use('/public', express.static('public')); // http://localhost:8888/images/tower.jpg
+
+// app.get('/', (req, res, next) => {
+//   res.sendFile(path.join(__dirname, 'pages/index.html'));
+// });
+
+// app.get('/main.js', (req,res,next) => {
+
+// })
+
+// app.get('/', (req, res, next) => {
+//   const query = req.query;
+//   const { name, age, address } = req.query;
+//   // query.name;
+
+//   console.log(query);
+// });
+
+// /product/788899999
+// /product/100/shop/250
+app.get('/product/:productId/shop/:shopId', (req, res, next) => {
+  const params = req.params; // { productId: '100', shopId: '250'  }
+  console.log(params);
 });
-productRouter.post('/', (req, res, next) => {
-  try {
-    // connected to database  to create new product
-    // asynchronous operation
-  } catch (err) {
-    next(err);
-  }
+
+// /shop-mobile/axcvbn
+// app.get('/shop-mobile/:shopId', (req, res, next) => {});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/login.html'));
 });
-productRouter.put('/', (req, res, next) => {});
-productRouter.delete('/', (req, res, next) => {});
 
-app.use('/products', productRouter);
-
-app.use((req, res, next) => {});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(err.statusCode || 500).json({ aaaaaaa: err.message });
+app.post('/post-login', (req, res) => {
+  const body = req.body;
+  console.log(body.username);
+  console.log(typeof body.username);
+  console.log(body.password);
+  console.log(typeof body.password);
 });
 
 app.listen(8888, () => console.log('server running on port 8888'));
